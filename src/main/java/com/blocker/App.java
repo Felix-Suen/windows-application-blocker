72337525453541fc948b3847f0c03b8ee940a98d
Felix-Suen <felix.suen2000@gmail.com>
2021-05-21 19:59:43 -0700

    timer

--- src/main/java/com/blocker/App.java
+++ src/main/java/com/blocker/App.java
@@ -5,49 +5,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class App {
-    public HashSet<String> set = new HashSet<String>();
-
-    public void processes() {
-        try {
-            Process process = Runtime.getRuntime().exec("tasklist");
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line;
-            int counter = 0;
-            while ((line = reader.readLine()) != null) {
-                // get rid of useless lines in the command line
-                counter++;
-
-                if (counter > 6) {
-                    // create a hashset for O(1) search
-                    int i = line.indexOf(' ');
-                    String word = line.substring(0, i);
-                    System.out.println(word);
-                    this.set.add(word);
-                }
-
-            }
-        } catch (IOException ie) {
-            ie.printStackTrace();
-        }
-    }
-
-    public void kill (ArrayList<String> apps) {
-        try {
-            Runtime.getRuntime().exec("taskkill /IM " + "VALORANT-Win64-Shipping.exe" + " /F");
-            for (String app : apps) {
-                if (set.contains(app)) {
-                    Runtime.getRuntime().exec("taskkill /IM " + app + " /F");
-                    System.out.println(app + " is now blocked");
-                } else {
-                    System.out.println(app + " is not found");
-                }
-            }
-        } catch (Exception ie) {
-            ie.printStackTrace();
-        }
-    }
+public class App extends TimerTask {
+    //public HashSet<String> set = new HashSet<String>();
 
     public void findAndKill (ArrayList<String> apps) {
         try {
@@ -82,15 +44,21 @@ public class App {
         }
     }
 
-    public static void main(String[] args) {
-        App block = new App();
+    public void run() {
+        // App block = new App();
         ArrayList<String> apps = new ArrayList<String>();
         apps.add("Discord.exe");
         apps.add("Todoist.exe");
         apps.add("VALORANT.exe");
         apps.add("VALORANT-Win64-Shipping.exe");
-        block.findAndKill(apps);
-        // block.processes();
-        // block.kill(apps);
+
+        findAndKill(apps);
+    }
+
+    public static void main(String[] args) {
+        
+
+        Timer timer = new Timer();
+        timer.schedule(new App(), 0, 5000);
     }
 }
